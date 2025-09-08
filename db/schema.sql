24e3354f479aaee7d4d19d8de866afb2d4b174f6
Jason Lunz <znull@github.com>
2025-09-08 13:40:07 +0200

    drop id column

--- db/schema.sql
+++ db/schema.sql
@@ -1,4 +1,4 @@
 CREATE SEQUENCE seq_workout_id INCREMENT BY 1 MINVALUE 1 MAXVALUE 9223372036854775807 START 58 NO CYCLE;;
 CREATE TABLE wodsets(date DATE PRIMARY KEY, uuid UUID DEFAULT(gen_random_uuid()) UNIQUE);;
-CREATE TABLE workouts(id INTEGER DEFAULT(nextval('seq_workout_id')) PRIMARY KEY, wod_section VARCHAR, wod_title VARCHAR, workout_name VARCHAR, workout_description VARCHAR, wod_results_count INTEGER, wod_results_url VARCHAR, date DATE, updated_at TIMESTAMP, seq INTEGER, FOREIGN KEY (date) REFERENCES wodsets(date));;
+CREATE TABLE workouts(wod_section VARCHAR, wod_title VARCHAR, workout_name VARCHAR, workout_description VARCHAR, wod_results_count INTEGER, wod_results_url VARCHAR, date DATE, updated_at TIMESTAMP, seq INTEGER, FOREIGN KEY (date) REFERENCES wodsets(date));;
 
