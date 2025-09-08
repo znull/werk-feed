86a29d59f3d36de8a7ada219e10e11b531c3aab5
Jason Lunz <znull@github.com>
2025-09-08 14:42:15 +0200

    simplify db further

--- db/load.sql
+++ db/load.sql
@@ -1,2 +1,1 @@
-COPY wodsets FROM 'db/wodsets.csv' (FORMAT 'csv', force_not_null 'date', quote '"', delimiter ',', header 1);
-COPY workouts FROM 'db/workouts.csv' (FORMAT 'csv', quote '"', delimiter ',', header 1);
+COPY workouts FROM 'db/workouts.csv' (FORMAT 'csv', force_not_null ('date', 'seq'), quote '"', delimiter ',', header 1);
--- db/schema.sql
+++ db/schema.sql
@@ -1,4 +1,2 @@
-CREATE SEQUENCE seq_workout_id INCREMENT BY 1 MINVALUE 1 MAXVALUE 9223372036854775807 START 58 NO CYCLE;;
-CREATE TABLE wodsets(date DATE PRIMARY KEY, uuid UUID DEFAULT(gen_random_uuid()) UNIQUE);;
-CREATE TABLE workouts(wod_section VARCHAR, wod_title VARCHAR, workout_name VARCHAR, workout_description VARCHAR, wod_results_count INTEGER, wod_results_url VARCHAR, date DATE, updated_at TIMESTAMP, seq INTEGER, FOREIGN KEY (date) REFERENCES wodsets(date));;
+CREATE TABLE workouts(date DATE, seq INTEGER, wod_section VARCHAR, updated_at TIMESTAMP, wod_title VARCHAR, workout_name VARCHAR, wod_results_count INTEGER, wod_results_url VARCHAR, workout_description VARCHAR, PRIMARY KEY(date, seq));;
 
