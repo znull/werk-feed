ba774173f96ac1b490ae97eb59aa3da11c724e95
Jason Lunz <znull@github.com>
2025-09-21 13:50:27 +0200

    db: extract atom_entries table

--- db/load.sql
+++ db/load.sql
@@ -1,1 +1,2 @@
+COPY atom_entries FROM 'db/atom_entries.csv' (FORMAT 'csv', quote '"', delimiter ',', header 1);
 COPY workouts FROM 'db/workouts.csv' (FORMAT 'csv', force_not_null ('date', 'seq'), quote '"', delimiter ',', header 1);
--- db/schema.sql
+++ db/schema.sql
@@ -1,2 +1,3 @@
-CREATE TABLE workouts(date DATE, seq INTEGER, wod_section VARCHAR, updated_at TIMESTAMP, wod_title VARCHAR, workout_name VARCHAR, wod_results_count INTEGER, wod_results_url VARCHAR, workout_description VARCHAR, PRIMARY KEY(date, seq));;
+CREATE TABLE atom_entries(date DATE, csum VARCHAR, created_at TIMESTAMP, updated_at TIMESTAMP);;
+CREATE TABLE workouts(date DATE, seq INTEGER, wod_section VARCHAR, wod_title VARCHAR, workout_name VARCHAR, wod_results_count INTEGER, wod_results_url VARCHAR, workout_description VARCHAR, PRIMARY KEY(date, seq));;
 
